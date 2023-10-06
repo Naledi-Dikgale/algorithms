cddffe721473b436e4111ba068239c6493cb2542
naledi-dikgale <naledi22khosi@gmail.com>
2023-10-06 23:41:13 +0200

    balance brackets.js

---
+++ arrays/balance-brackets.js
@@ -0,0 +1,26 @@
+ // Initialize two arrays to hold non-zero and zero elements
+    // Loop through the input array
+   // If the element is non-zero, push it to nonZeroElements
+  // If the element is zero, push it to zeroElements
+  // Concatenate the non-zero and zero elements arrays
+  // Test the function with an example array
+
+  function moveZerosToEnd(arr) {
+    const nonZeroElements = [];
+    const zeroElements = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== 0) {
+        nonZeroElements.push(arr[i]);
+      } else {
+        zeroElements.push(arr[i]);
+      }
+    }
+    const result = nonZeroElements.concat(zeroElements);
+    return result;
+  }
+  
+  const inputArray = [1, 0, 2, 0, 3, 0, 4, 5, 0];
+  const resultArray = moveZerosToEnd(inputArray);
+  console.log(resultArray); // Output: [1, 2, 3, 4, 5, 0, 0, 0, 0]
+  
