0184b7e59b4fff70efe1221c24b504d599f3b55e
naledi-dikgale <naledi22khosi@gmail.com>
2023-10-14 19:42:39 +0200

    Anagram checker

---
+++ arrays/angram-checker.js
@@ -0,0 +1,23 @@
+
+// Return true if the two given strings are anagrams. Otherwise, return false.
+// A word is an anagram of another word if both use the same letters in the same quantity, but arranged differently.
+// For example:
+// friend and Finder are anagrams.
+// The Morse Code and Here come dots are anagrams as well.
+// Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for anagrams.
+
+module.exports = function (stringA, stringB) {
+  return (
+    stringA
+      .toLowerCase()
+      .match(/[a-zA-Z]/g)
+      .sort()
+      .join('')
+    ===
+    stringB
+      .toLowerCase()
+      .match(/[a-zA-Z]/g)
+      .sort()
+      .join('')
+  );
+};
