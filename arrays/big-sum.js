959d007242f14937381c443a11d359cf13debddc
naledi-dikgale <naledi22khosi@gmail.com>
2023-10-29 20:39:10 +0200

    big sum

---
+++ arrays/big-sum.js
@@ -0,0 +1,76 @@
+
+// Function Description
+
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+// aVeryBigSum has the following parameter(s):
+
+// int ar[n]: an array of integers .
+// Return
+
+// long: the sum of all array elements
+// Input Format
+
+// The first line of the input consists of an integer .
+// The next line contains  space-separated integers contained in the array.
+
+// Output Format
+
+// Return the integer sum of the elements in the array.
+
+// Constraints
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum(ar) {
+    // Write your code here
+    let sum = 0;
+    
+    for (let i = 0; i < ar.length; i++) {
+        sum += ar[i];
+    }
+    
+    return sum;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = aVeryBigSum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
