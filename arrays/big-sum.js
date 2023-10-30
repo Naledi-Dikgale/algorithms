9b41e725641b746d3219e6254ff3859706cdea30
naledi-dikgale <naledi22khosi@gmail.com>
2023-10-30 23:05:55 +0200

    big-sum.js

--- arrays/big-sum.js
+++ arrays/big-sum.js
@@ -2,23 +2,17 @@
 // Function Description
 
 // Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
-
 // aVeryBigSum has the following parameter(s):
-
 // int ar[n]: an array of integers .
 // Return
-
 // long: the sum of all array elements
 // Input Format
-
 // The first line of the input consists of an integer .
 // The next line contains  space-separated integers contained in the array.
-
 // Output Format
-
 // Return the integer sum of the elements in the array.
-
 // Constraints
+
 'use strict';
 
 const fs = require('fs');
