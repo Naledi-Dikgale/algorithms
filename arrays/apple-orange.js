fda6147c4e4263bcf9575ab3aa6737a82d05acee
naledi-dikgale <naledi22khosi@gmail.com>
2023-12-13 21:00:35 +0200

    Challange.js

---
+++ arrays/apple-orange.js
@@ -0,0 +1,18 @@
+// Function Description [https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true]
+
+// Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+// countApplesAndOranges has the following parameter(s):
+// s: integer, starting point of Sam's house location.
+// t: integer, ending location of Sam's house location.
+// a: integer, location of the Apple tree.
+// b: integer, location of the Orange tree.
+// apples: integer array, distances at which each apple falls from the tree.
+// oranges: integer array, distances at which each orange falls from the tree.
+// Input Format
+
+// The first line contains two space-separated integers denoting the respective values of  and .
+// The second line contains two space-separated integers denoting the respective values of  and .
+// The third line contains two space-separated integers denoting the respective values of  and .
+// The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
+// The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
