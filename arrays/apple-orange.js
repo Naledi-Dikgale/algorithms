80449040feded521eed43e71100ad8e33d3e08f6
naledi-dikgale <naledi22khosi@gmail.com>
2023-12-15 06:02:39 +0200

    Apples.js

--- arrays/apple-orange.js
+++ arrays/apple-orange.js
@@ -17,6 +17,15 @@
 // The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
 // The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
 
+// Sample Input
+// 7 11
+// 5 15
+// 3 2
+// -2 2 1
+// 5 -6
+// Sample Output
+// 1
+// 1
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // Write your code here
   let appleCount = 0;
