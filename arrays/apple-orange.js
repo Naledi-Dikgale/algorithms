0a3f96660215f1da9d672cfec6c28fada37109d1
naledi-dikgale <naledi22khosi@gmail.com>
2023-12-16 21:04:20 +0200

    Description

--- arrays/apple-orange.js
+++ arrays/apple-orange.js
@@ -11,11 +11,11 @@
 // oranges: integer array, distances at which each orange falls from the tree.
 // Input Format
 
-// The first line contains two space-separated integers denoting the respective values of  and .
-// The second line contains two space-separated integers denoting the respective values of  and .
-// The third line contains two space-separated integers denoting the respective values of  and .
-// The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
-// The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+// The first line contains two space-separated integers denoting the respective values of s and t.
+// The second line contains two space-separated integers denoting the respective values of a and b.
+// The third line contains two space-separated integers denoting the respective values of m and n.
+// The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point a.
+// The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point b.
 
 // Sample Input
 // 7 11
