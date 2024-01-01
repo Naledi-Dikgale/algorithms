b9a3a2dc5be06fbc81deebdb47ea31ef0ce188f9
naledi-dikgale <naledi22khosi@gmail.com>
2024-01-01 18:55:01 +0200

    README.md

--- arrays/fizzbuzz.js
+++ arrays/fizzbuzz.js
@@ -1,3 +1,11 @@
+// # FizzBuzz
+
+// - Write a program in `fizzbuzz.js` that prints an array of integers from 1 to 100.
+// - For multiples of 3, show `"Fizz"` instead of the number.
+// - For multiples of 5, show `"Buzz"` instead of the number.
+// - For multiples of 3 and 5, show `"FizzBuzz"` instead of the number.
+// - Return an array containing the results based on the rules above.
+
 module.exports = function fizzBuzz() {
     let result = [];
     for (let i = 1; result.length < 100; i++) {
