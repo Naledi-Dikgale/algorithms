9643a29f9e0ca3885a87fc3fb3b7c3108a9adbf9
naledi-dikgale <naledi22khosi@gmail.com>
2024-01-20 20:41:57 +0200

    Update

--- arrays/filter-elements.js
+++ arrays/filter-elements.js
@@ -12,9 +12,9 @@
 // Example 3:
 // Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
 // Output: [-2,0,1,2]
+
 // Explanation:
 // Falsey values such as 0 should be filtered out
-
 // Constraints:
 // 0 <= arr.length <= 1000
 // -109 <= arr[i] <= 109
