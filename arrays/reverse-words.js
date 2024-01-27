1a565a338bd21bf30b0471549ed3245c987bf543
naledi-dikgale <naledi22khosi@gmail.com>
2024-01-27 18:17:01 +0200

    Reverse-words.js

---
+++ arrays/reverse-words.js
@@ -0,0 +1,9 @@
+// You are tasked with creating a JavaScript function that reverses the order of words in 
+// a given string while preserving the order of characters within each word.
+// Your function should take a single string as input and return
+// a new string with the words reversed.
+// For example, given the input string "hello world", your function should return "world hello".
+
+// Solution 1
+
+fu
