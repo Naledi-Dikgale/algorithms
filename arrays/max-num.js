e4254339cc1487cf58222a814fc3544412f6ad67
naledi-dikgale <naledi22khosi@gmail.com>
2024-02-08 22:01:17 +0200

    maximum number

---
+++ arrays/max-num.js
@@ -0,0 +1,6 @@
+// Initialize a variable max to store the maximum value and set it to negative infinity.
+// Iterate through each element of the array.
+// Compare each element with the current maximum (max).
+// If the current element is greater than max, update max with the value of the current element.
+// After iterating through the entire array, max will hold the maximum value.
+// Return max.
