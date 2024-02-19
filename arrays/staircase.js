bcf85b60df4c44888ac24f03f632faabadab071c
naledi-dikgale <naledi22khosi@gmail.com>
2024-02-19 20:45:00 +0200

    Staircase description

---
+++ arrays/staircase.js
@@ -0,0 +1,11 @@
+// Its base and height are both equal to . It is drawn using # symbols and spaces. 
+// The last line is not preceded by any spaces.
+// Write a program that prints a staircase of size .
+// Function Description
+// Complete the staircase function in the editor below.
+// staircase has the following parameter(s):
+// int n: an integer
+// Print
+// Print a staircase as described above.
+// Input Format
+// A single integer, , denoting the size of the staircase.
