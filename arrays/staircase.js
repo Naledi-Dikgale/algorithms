0ed09200fa6ac957c9cd47991118e42d01838097
naledi-dikgale <naledi22khosi@gmail.com>
2024-02-20 08:35:42 +0200

    Function

--- arrays/staircase.js
+++ arrays/staircase.js
@@ -9,3 +9,42 @@
 // Print a staircase as described above.
 // Input Format
 // A single integer, , denoting the size of the staircase.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+    // Write your code here
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
