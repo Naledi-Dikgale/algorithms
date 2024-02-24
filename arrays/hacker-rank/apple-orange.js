273c555bb1f210d8a9b6241b6469d51e57759610
naledi-dikgale <naledi22khosi@gmail.com>
2024-02-24 16:33:11 +0200

    Organise folders

---
+++ arrays/hacker-rank/apple-orange.js
@@ -0,0 +1,50 @@
+// Function Description [https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true]
+
+// Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+// countApplesAndOranges has the following parameter(s):
+// s: integer, starting point of Sam's house location.
+// t: integer, ending location of Sam's house location.
+// a: integer, location of the Apple tree.
+// b: integer, location of the Orange tree.
+// apples: integer array, distances at which each apple falls from the tree.
+// oranges: integer array, distances at which each orange falls from the tree.
+// Input Format
+
+// The first line contains two space-separated integers denoting the respective values of s and t.
+// The second line contains two space-separated integers denoting the respective values of a and b.
+// The third line contains two space-separated integers denoting the respective values of m and n.
+// The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point a.
+// The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point b.
+
+// Sample Input
+// 7 11
+// 5 15
+// 3 2
+// -2 2 1
+// 5 -6
+// Sample Output
+// 1
+// 1
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  // Write your code here
+  let appleCount = 0;
+  let orangeCount = 0;
+
+  apples.forEach(apple => {
+      const applePosition = a + apple;
+      if (applePosition >= s && applePosition <= t) {
+          appleCount++;
+      }
+  });
+
+  oranges.forEach(orange => {
+      const orangePosition = b + orange;
+      if (orangePosition >= s && orangePosition <= t) {
+          orangeCount++;
+      }
+  });
+
+  console.log(appleCount);
+  console.log(orangeCount);
+}
