394dcf6ce91f64b752867946c64f37ac279ffedf
naledi-dikgale <naledi22khosi@gmail.com>
2024-03-12 21:41:58 +0200

    Add inline comments

--- unknown-source/gh-classroom/fizzbuzz.js
+++ unknown-source/gh-classroom/fizzbuzz.js
@@ -7,20 +7,27 @@
 // - Return an array containing the results based on the rules above.
 
 module.exports = function fizzBuzz() {
-    let result = [];
-    for (let i = 1; result.length < 100; i++) {
-      if ((i % 3 == 0) && (i % 5 == 0)) {
-        result.push("FizzBuzz");
-      }
-      else if (i % 3 == 0) {
-        result.push("Fizz");
-      }
-      else if (i % 5 == 0) {
-        result.push("Buzz");
-      }
-      else {
-        result.push(i);
-      }
+  // Initialize an empty array to store the result
+  let result = [];
+  // Loop through numbers from 1 to 100
+  for (let i = 1; result.length < 100; i++) {
+    // Check if the number is a multiple of both 3 and 5
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+      result.push("FizzBuzz"); // If true, push "FizzBuzz" to the result array
     }
-    return result;
+    // Check if the number is a multiple of 3
+    else if (i % 3 == 0) {
+      result.push("Fizz"); // If true, push "Fizz" to the result array
+    }
+    // Check if the number is a multiple of 5
+    else if (i % 5 == 0) {
+      result.push("Buzz"); // If true, push "Buzz" to the result array
+    }
+    // If none of the above conditions are met, push the number itself to the result array
+    else {
+      result.push(i);
+    }
   }
+  // Return the resulting array
+  return result;
+}
