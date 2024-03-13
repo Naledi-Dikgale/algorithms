d89eeeaf473942619a48abf6200a132ca80f6a7f
naledi-dikgale <naledi22khosi@gmail.com>
2024-03-13 15:50:49 +0200

    cake.js

---
+++ arrays/hacker-rank/b-day-cake.js
@@ -0,0 +1,10 @@
+// You are in charge of the cake for a child's birthday. 
+// You have decided the cake will have one candle for each year of their total age.
+// They will only be able to blow out the tallest of the candles.
+// Count  how many candles are tallest.
+
+// Example
+// candles = [4,4,1,3]
+// The maximum height candles are 4 units high. There are 2 of them, so return 2.
+
+
