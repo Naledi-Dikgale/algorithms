99db1e4c30a9bc3a142db4ba1069e6b4b4b26605
naledi-dikgale <naledi22khosi@gmail.com>
2024-03-16 21:48:59 +0200

    Bd-cake.js

--- arrays/hacker-rank/b-day-cake.js
+++ arrays/hacker-rank/b-day-cake.js
@@ -11,6 +11,42 @@
 // Complete the function birthdayCakeCandles in the editor below.
 // Complete the function birthdayCakeCandles in the editor below.
 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+
+}
+
 
 function birthdayCakeCandles(candles) {
   // Find the maximum height of the candles
@@ -23,6 +59,18 @@ function birthdayCakeCandles(candles) {
 }
 
 
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
+  const candlesCount = parseInt(readLine().trim(), 10);
+
+  const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+  const result = birthdayCakeCandles(candles);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
 
 
